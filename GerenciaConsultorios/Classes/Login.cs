4c2678351d0359aab965a59b9f34b72238c1b78f
Gabrielitalo <gabrielitaloalv@gmail.com>
2020-09-02 19:41:19 -0300

    Gabriel
    Implementando funções da tela de login

---
+++ GerenciaConsultorios/Classes/Login.cs
@@ -0,0 +1,104 @@
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace GerenciaConsultorios.Classes
+{
+    public class Login
+    {
+        public int Pk { get; set; }
+        public string Nome { get; set; }
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+        public int TipoUsuario { get; set; }
+
+        private MySqlDataAdapter mAdapter;
+        DbCon con = new DbCon();
+
+
+        public string retornarUsuarios(int tipo, int pkUsuario = 0)
+        {
+            DataSet ds = new DataSet();
+            List<Login> login = new List<Login>();
+            string comando = "";
+            if (tipo == 0)
+            {
+                comando = "Select * From CadUSuarios Order by Nome;";
+            }
+            else
+            {
+                comando = "Select * From CadUSuarios Where Pk = " + pkUsuario + ";";
+
+            }
+
+            // Executando comando
+            mAdapter = new MySqlDataAdapter(comando, con.connectionStringMySql);
+            // Populando o data set
+            mAdapter.Fill(ds);
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                int i = 0;
+                for (i = 0; i <= ds.Tables[0].Rows.Count; i++)
+                {
+                    login.Add
+                        (
+                        new Login
+                            {
+                                Pk = Convert.ToInt32(ds.Tables[0].Rows[i]["Pk"]),
+                                Nome = ds.Tables[0].Rows[i]["Nome"].ToString(),
+                                Usuario = ds.Tables[0].Rows[i]["Usuario"].ToString(),
+                                Senha = ds.Tables[0].Rows[i]["Senha"].ToString(),
+                                TipoUsuario = Convert.ToInt32(ds.Tables[0].Rows[i]["TipoUsuario"].ToString())
+                            }
+                        );
+                }
+            }
+
+            return JsonConvert.SerializeObject(login, Formatting.Indented);
+        }
+        public string cadastrarNovoUsuario(string nome, string usuario, string senha, string tipoUsuario)
+        {
+            DataSet ds = new DataSet();
+            string comando = @"Insert Into Login (Nome, Usuario, Senha, TipoUsuario) " +
+                               "Select " + nome + ", " + usuario + ", " + senha + ", " + tipoUsuario + ";";
+            // Executando comando
+            mAdapter = new MySqlDataAdapter(comando, con.connectionStringMySql);
+            // Populando o data set
+            mAdapter.Fill(ds);
+
+
+            return "OK";
+        }
+
+        public string alterarUsuario(int pkUsuario, string nome, string usuario, string senha, string tipoUsuario)
+        {
+            DataSet ds = new DataSet();
+            string comando = @"Update Login" +
+                               "Set " + nome + ", " + usuario + ", " + senha + ", " + tipoUsuario + "" +
+                               "Where (Pk = " + pkUsuario.ToString() + ");";
+            // Executando comando
+            mAdapter = new MySqlDataAdapter(comando, con.connectionStringMySql);
+            // Populando o data set
+            mAdapter.Fill(ds);
+
+            return "OK";
+        }
+
+        public string excluirUsuario(int pkUsuario)
+        {
+            DataSet ds = new DataSet();
+            string comando = "Delete From Login Where (Pk = " + pkUsuario + ")";
+            // Executando comando
+            mAdapter = new MySqlDataAdapter(comando, con.connectionStringMySql);
+            // Populando o data set
+            mAdapter.Fill(ds);
+
+            return "OK";
+        }
+    }
+}
--- GerenciaConsultorios/Controllers/ConsultorioController.cs
+++ GerenciaConsultorios/Controllers/ConsultorioController.cs
@@ -11,83 +11,116 @@ using Newtonsoft.Json;
 
 namespace GerenciaConsultorios.Controllers
 {
-  public class ConsultorioController : ApiController
-  {
-    Consultorio consultorio = new Consultorio();
-    Medicos medicos = new Medicos();
-
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    // Consultórios
-
-    [AcceptVerbs("GET")]
-    [Route("api/RetornaConsultorios")]
-    public string retornaConsultorios(int tipo, int pkConsultorio = 0)
+    public class ConsultorioController : ApiController
     {
-      return consultorio.retornarConsultorios(tipo, pkConsultorio);
+        //---------------------------------------------------------------------------------------------------------------------------------------
+        // Classes instanciadas 
+
+        Login login = new Login();
+        Consultorio consultorio = new Consultorio();
+        Medicos medicos = new Medicos();
+
+        //---------------------------------------------------------------------------------------------------------------------------------------
+        // Login
+        [AcceptVerbs("GET")]
+        [Route("api/RetornarUsuarios")]
+        public string retornarUsuarios(int tipo, int pkUsuario)
+        {
+            return login.retornarUsuarios(tipo, pkUsuario); 
+        }
+
+        [AcceptVerbs("POST")]
+        [Route("api/CadastrarUsuario")]
+        public string cadastrarUsuario(string nome, string usuario, string senha, string tipoUsuario)
+        {
+            return login.cadastrarNovoUsuario(nome, usuario, senha, tipoUsuario);
+        }
+
+        [AcceptVerbs("DELETE")]
+        [Route("api/ExcluirUsuario")]
+        public string excluirUsuario(int pkUsuario)
+        {
+            return login.excluirUsuario(pkUsuario);
+        }
+
+        [AcceptVerbs("PUT")]
+        [Route("api/AlterarUsuario")]
+        public string alterarUsuario(int pkUsuario, string nome, string usuario, string senha, string tipoUsuario)
+        {
+            return login.alterarUsuario(pkUsuario, nome, usuario, senha, tipoUsuario);
+        }
+        //---------------------------------------------------------------------------------------------------------------------------------------
+        // Consultórios
+
+        [AcceptVerbs("GET")]
+        [Route("api/RetornaConsultorios")]
+        public string retornaConsultorios(int tipo, int pkConsultorio = 0)
+        {
+            return consultorio.retornarConsultorios(tipo, pkConsultorio);
+        }
+
+        [AcceptVerbs("POST")]
+        [Route("api/CadastrarConsultorio")]
+        public string cadastrarConsultorio(string nome, string endereco, string telefone)
+        {
+            return consultorio.cadastrar(nome, endereco, telefone);
+        }
+
+        [AcceptVerbs("DELETE")]
+        [Route("api/ExluirConsultorio")]
+        public void exluirConsultorio(int pkConsultorio)
+        {
+            consultorio.excluir(pkConsultorio);
+        }
+
+        [AcceptVerbs("PUT")]
+        [Route("api/AlterarConsultorio")]
+        public void alterarConsultorio(int pkConsultorio, string nome, string endereco, string telefone)
+        {
+            consultorio.alterar(pkConsultorio, nome, endereco, telefone);
+        }
+
+        // Fim consultórios
+        //---------------------------------------------------------------------------------------------------------------------------------------
+        // Médicos
+
+        [AcceptVerbs("GET")]
+        [Route("api/RetornaMedicos")]
+        public string retornaMedicos(int tipo, int pkMedico = 0, int pkConsultorio = 0)
+        {
+            return medicos.retornarMedicos(tipo, pkMedico, pkConsultorio);
+        }
+
+        [AcceptVerbs("POST")]
+        [Route("api/CadastrarMedico")]
+        public string cadastrarMedico(string crm, string nome, string telefone, string valorConsulta)
+        {
+            return medicos.cadastrar(nome, telefone, valorConsulta, crm);
+        }
+
+        [AcceptVerbs("DELETE")]
+        [Route("api/ExluirMedico")]
+        public void exluirMedico(int pkMedico)
+        {
+            medicos.excluir(pkMedico);
+        }
+
+        [AcceptVerbs("PUT")]
+        [Route("api/AlterarMedico")]
+        public void alterarMedico(int pkMedico, string crm, string nome, string telefone, string valorConsulta)
+        {
+            medicos.alterar(pkMedico, crm, nome, telefone, valorConsulta);
+        }
+
+
+        [AcceptVerbs("POST")]
+        [Route("api/GerenciarMedicoConsultorio")]
+        public string gerenciarMedicoConsultorio(int tipo, int pkConsultorio, int pkMedico)
+        {
+            return medicos.vincularMedicoConsultorio(tipo, pkConsultorio, pkMedico);
+        }
+        // Fim médicos
+        //---------------------------------------------------------------------------------------------------------------------------------------
+
     }
-
-    [AcceptVerbs("POST")]
-    [Route("api/CadastrarConsultorio")]
-    public string cadastrarConsultorio(string nome, string endereco, string telefone)
-    {
-      return consultorio.cadastrar(nome, endereco, telefone);
-    }
-
-    [AcceptVerbs("DELETE")]
-    [Route("api/ExluirConsultorio")]
-    public void exluirConsultorio(int pkConsultorio)
-    {
-      consultorio.excluir(pkConsultorio);
-    }
-
-    [AcceptVerbs("PUT")]
-    [Route("api/AlterarConsultorio")]
-    public void alterarConsultorio(int pkConsultorio, string nome, string endereco, string telefone)
-    {
-      consultorio.alterar(pkConsultorio, nome, endereco, telefone);
-    }
-
-    // Fim consultórios
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    // Médicos
-
-    [AcceptVerbs("GET")]
-    [Route("api/RetornaMedicos")]
-    public string retornaMedicos(int tipo, int pkMedico = 0, int pkConsultorio = 0)
-    {
-      return medicos.retornarMedicos(tipo, pkMedico, pkConsultorio);
-    }
-
-    [AcceptVerbs("POST")]
-    [Route("api/CadastrarMedico")]
-    public string cadastrarMedico(string crm, string nome, string telefone, string valorConsulta)
-    {
-      return medicos.cadastrar(nome, telefone, valorConsulta, crm);
-    }
-
-    [AcceptVerbs("DELETE")]
-    [Route("api/ExluirMedico")]
-    public void exluirMedico(int pkMedico)
-    {
-      medicos.excluir(pkMedico);
-    }
-
-    [AcceptVerbs("PUT")]
-    [Route("api/AlterarMedico")]
-    public void alterarMedico(int pkMedico, string crm, string nome, string telefone, string valorConsulta)
-    {
-      medicos.alterar(pkMedico, crm, nome, telefone, valorConsulta);
-    }
-
-
-    [AcceptVerbs("POST")]
-    [Route("api/GerenciarMedicoConsultorio")]
-    public string gerenciarMedicoConsultorio(int tipo, int pkConsultorio, int pkMedico)
-    {
-      return medicos.vincularMedicoConsultorio(tipo, pkConsultorio, pkMedico);
-    }
-    // Fim médicos
-    //---------------------------------------------------------------------------------------------------------------------------------------
-
-  }
 }
