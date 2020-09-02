7451037dea39917897e2f2c73f0e72e2211f1702
Gabrielitalo <gabrielitaloalv@gmail.com>
2020-09-02 19:55:49 -0300

    Gabriel
    Padronizando indentamento de código

--- GerenciaConsultorios/Classes/DbCon.cs
+++ GerenciaConsultorios/Classes/DbCon.cs
@@ -5,9 +5,9 @@ using System.Web;
 
 namespace GerenciaConsultorios.Classes
 {
-    public class DbCon
-    {
-        public string connectionStringMySql = "Server=sql199.main-hosting.eu;Database=u594043994_GerenciarMedic;Uid=u594043994_guest;Pwd=Guest321";
+  public class DbCon
+  {
+    public string connectionStringMySql = "Server=sql199.main-hosting.eu;Database=u594043994_GerenciarMedic;Uid=u594043994_guest;Pwd=Guest321";
 
-    }
+  }
 }
--- GerenciaConsultorios/Classes/MedicoData/MovMedicos.cs
+++ GerenciaConsultorios/Classes/MedicoData/MovMedicos.cs
@@ -5,11 +5,11 @@ using System.Web;
 
 namespace GerenciaConsultorios.Classes.MedicoData
 {
-    public class MovMedicos
-    {
-        public int Pk { get; set; }
-        public int FkCadMedicos { get; set; }
-        public int FkCadConsultorio { get; set; }
+  public class MovMedicos
+  {
+    public int Pk { get; set; }
+    public int FkCadMedicos { get; set; }
+    public int FkCadConsultorio { get; set; }
 
-    }
+  }
 }
